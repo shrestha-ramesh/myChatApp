84e739649aa4947e4ea0812145a8496d4f96fc6d
Ramesh Shrestha <shresthashare@gmail.com>
2020-08-07 15:41:04 -0500

    Added necessary packages. Need to npm install

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const { generateMessage }= require('./utils/messages');
+const Filter = require('bad-words');
+const io = socketio(server);
