36d403c8f281bbf2a02b0846544a71b39a1b2c5d
Ramesh Shrestha <shresthashare@gmail.com>
2020-08-08 17:35:31 -0500

    added chat.js file to communicate with the index.js

---
+++ js/chat.js
@@ -0,0 +1,35 @@
+const $form=document.querySelector('form');
+const $input=document.querySelector('input');
+const $location_btn=document.querySelector('#send-location');
+const $messages=document.querySelector('#messages');
+
+const messageTemplate=document.querySelector('#message-template').innerHTML;
+const locationTemplate=document.querySelector('#location-template').innerHTML;
+const sidebarTemplate=document.querySelector('#sidebar-template').innerHTML;
+
+const {username,room}=Qs.parse(location.search,{igonreQueryPrefix:true});
+const autoscroll=()=>{
+    const $newMessage = $messages.lastElementChild;
+    const newMessageStyles=getComputedStyle($newMessage);
+    const newMessageMargin=parseInt(newMessageStyles.marginBottom);
+    const newMessageHeight=$newMessage.offsetHeight+newMessageMargin;
+
+    const visibleHeight = $messages.offsetHeight;
+    const containerHeight = $messages.scrollHeight;
+    const scrollOffet=$messages.scrollTop+visibleHeight;
+    if(containerHeight-newMessageHeight <= scrollOffet){
+        $messages.scrollTop = $messages.scrollHeight;
+    }
+}
+
+const socket=io()
+
+socket.on('message', (message)=>{
+    const html = Mustache.render(messageTemplate,{
+        username:message.username,
+        message:message.text,
+        createdAt:moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html);
+    autoscroll();
+})
