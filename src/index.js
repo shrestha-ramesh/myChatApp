87ceaa0ba182d4e0e911664819b081bd52044ce5
Ramesh Shrestha <shresthashare@gmail.com>
2020-08-08 09:20:48 -0500

    check the call back fuction and make sure all the backend will perform well

--- src/index.js
+++ src/index.js
@@ -12,3 +12,17 @@ const io = socketio(server);
 const {addUser, removeUser, getUser, getUsersInRoom } = reqire('/utils/users');
 const publicDirectory=path.join(__dirname,'..');
 app.use(express.static(publicDirectory));
+io.on('connection',(socket)=>{
+    socket.on('join',(options,callback)=>{
+        const { error, user }=addUser({id:socket.id,...options})
+        if(error){ return callback(error);}
+        socket.join(user.room);
+        socket.emit('message', generateMessage('Welcome'));
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username}has joined.`));
+        io.to(user.room).emit('roomData',{ 
+            room: user.room,
+            Users:getUsersInRoom(user.room)
+        });
+        callback()
+    })
+});
